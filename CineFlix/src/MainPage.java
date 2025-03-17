b61979f2ea6d515c691977ac986d297ee2dee199
golprit <146406532+golprit@users.noreply.github.com>
2025-03-17 18:35:02 +0530

    Project design

--- CineFlix/src/MainPage.java
+++ CineFlix/src/MainPage.java
@@ -14,7 +14,7 @@ public class MainPage extends JFrame {
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
-			public void run() {
+			public void run() {  
 				try {
 					MainPage frame = new MainPage();
 					frame.setVisible(true);
